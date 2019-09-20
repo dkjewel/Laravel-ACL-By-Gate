94cf029152a223f87c0d56c3ae2c35339ce4b6bc
Didarul Karim Jewel <m.diarulkarimjewel@gmail.com>
2019-09-20 09:57:20 +0600

    user migration chnaged

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('user_type')->default(0);
+            $table->string('user_type')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
